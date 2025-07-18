df045bfc693787ad99c1d213a127590f410ffd59
paulo henriq <paulohsantos281096@gmail.com>
2025-07-18 10:58:43 -0300

    add: cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('cypress')
 
-module.exports = defineConfig({
+module.exports = defineConfig( {
+   projectId: "2jm9b8",
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 240000,
   requestTimeout: 15000,
   responseTimeout: 30000,
   e2e: {},
+
 })
