1c08c714f75f4e5b6607a22bb91f7f676cf87c34
Paulo Henrique <paulohrq_96@hotmail.com>
2025-07-18 01:09:08 -0300

    url

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,5 @@ module.exports = defineConfig({
   pageLoadTimeout: 240000,
   requestTimeout: 15000,
   responseTimeout: 30000,
-  e2e: {
-    baseUrl: 'https://www.saucedemo.com',
-  },
+  e2e: {},
 })
