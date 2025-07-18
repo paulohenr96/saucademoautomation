24122951abec9e02e388e7d1ed1b2dff72628cb1
Paulo Henrique <paulohrq_96@hotmail.com>
2025-07-18 00:39:57 -0300

    add: pageloadtimeout

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   defaultCommandTimeout: 10000,
-  pageLoadTimeout: 120000,
+  pageLoadTimeout: 240000,
   requestTimeout: 15000,
   responseTimeout: 30000,
   e2e: {
